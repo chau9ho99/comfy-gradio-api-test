41c2ef2f41c1429b24c2036d20d920e13544c8d7
chau9ho99 <155665911+chau9ho99@users.noreply.github.com>
2024-03-01 16:31:52 +0800

    Update light.py

--- light.py
+++ light.py
@@ -47,8 +47,8 @@ def get_latest_image(folder):
     image_files.sort(key=lambda x: os.path.getmtime(os.path.join(folder, x)))
     latest_image = os.path.join(folder, image_files[-1]) if image_files else None
     return latest_image
+def generate_image(input_text, style_choice="photo-hdr", use_base_style=False, aspect_ratio=None)
 
-def generate_image(input_text, style_choice="photo-hdr", use_base_style=False):
     with open("light.json", "r") as file_json:
         prompt = json.load(file_json)
     prompt["3"]["inputs"]["seed"] = random.randint(1, 1500000)
